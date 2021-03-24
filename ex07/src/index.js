dd7a7b938f2479bcd28a051ca7e00fb0ea3d6734
alko8910 <almedinemkic@gmail.com>
2021-03-24 09:05:39 +0100

    Mala izmjena

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,3 +1,3 @@
 const person = require('./nameFunctions.js');
 
-console.log(person.firstName("John") + " " + person.lastName("Doe"));
+console.log(`${person.firstName("John")} ${person.lastName("Doe")}`);
